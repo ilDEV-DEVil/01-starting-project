5daec9a5d22f06b52ee3ced26eca5f2929e5338c
Nimesh <nimesh.palakuttige@globant.com>
2025-02-11 12:46:21 +0100

    feat: introduces Input and input signal

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, Signal, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,11 +8,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath: Signal<String> = computed(() => 'assets/users/'+ this.selectedUser().avatar);
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  // get imagePath(): string {
+  //   return `assets/users/${this.avatar}`
+  // }
+
+  imagePath = computed(() => {
+      return `assets/users/${this.avatar()}`
+  })
+  
+  
+  onSelectUser() {};
 }
