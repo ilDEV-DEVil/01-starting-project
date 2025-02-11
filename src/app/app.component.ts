b3b51d06c428c7146ea4306b8e21448c6bc3359c
Nimesh <nimesh.palakuttige@globant.com>
2025-02-11 17:29:30 +0100

    feat: adds tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from './components/tasks/tasks.component';
 import { UserComponent } from "./components/user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,7 +15,13 @@ export class AppComponent {
 
   users = DUMMY_USERS;
 
+  selectedUserId: string = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId)
+  }
+
   handleUserSelected(id: string) {
-    console.log(id);
+    this.selectedUserId = id;
   }
 }
